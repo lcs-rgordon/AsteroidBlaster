bbdab77310dc52f350aba23caf11ac61d87ef288
Russell Gordon <rgordon@lcs.on.ca>
2018-01-14 08:15:04 -0500

    Initial Commit

---
+++ AsteroidBlaster/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AsteroidBlaster
+//
+//  Created by Russell Gordon on 1/14/18.
+//  Copyright © 2018 Russell Gordon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
